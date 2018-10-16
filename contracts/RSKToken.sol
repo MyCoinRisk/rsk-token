b42c35ca6ffd0e69d862dd3f42ecc22aeae81287
liuyao <liuyao@ejoy.com>
2018-10-16 18:31:33 +0800

    fix bug

--- contracts/RSKToken.sol
+++ contracts/RSKToken.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.23;
 
-import "zeppelin/token/VestedToken.sol"
+import "zeppelin/token/VestedToken.sol";
 
 contract RSKToken is VestedToken {
-    string public constant name = "RSKCoin"
-    string public constant symbol = "RSK"
+    string public constant name = "RSKCoin";
+    string public constant symbol = "RSK";
     uint8 public constant decimals = 18;
 
     uint256 public constant INITIAL_SUPPLY = 20000 * (10 ** uint256(decimals));
