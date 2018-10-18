042229346b1df821639c3ab39b183b7c284be69e
liuyao <liuyao@ejoy.com>
2018-10-18 16:49:13 +0800

    fix bug

--- contracts/RSKToken.sol
+++ contracts/RSKToken.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.23;
 
 import "zeppelin/token/VestedToken.sol";
+import "zeppelin/ownership/Ownable.sol";
 
-contract RSKToken is VestedToken {
+contract RSKToken is VestedToken, Ownable {
     string public constant name = "RSKCoin";
     string public constant symbol = "RSK";
     uint8 public constant decimals = 18;
@@ -11,11 +12,39 @@ contract RSKToken is VestedToken {
 
 
     /**
-      * Constructor that gives msg.sender all of existing tokens
-      */
+     * Constructor that gives msg.sender all of existing tokens
+     */
     function RSKToken() {
         totalSupply = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
     }
+
+    /**
+     * Batch transfer onlyOwner
+     */
+    function batchTransfer(address[] _tos, uint256[] _vals) onlyOwner public returns (bool) {
+        require(_tos.length > 0);
+        require(_tos.length == _vals.length);
+
+        // Transfer
+        for(uint256 i = 0; i < _tos.length; i++) {
+            if (_vals[i] > 0) {
+                transfer(_tos[i], _vals[i]);
+                // transfer(_tos[i], _vals[i] * (10 ** uint256(decimals)));
+            }
+            // transfer(_tos[i], balanceOf(owner)/_tos.length); 
+        }
+        return true;
+    }
+
+    // test balanceOf
+    function testBalanceOf() public constant returns (uint256) {
+        return balanceOf(this);
+    }
+
+    function ownerBalanceOf() public constant returns (uint256) {
+        return balanceOf(owner);
+    }
+
 }
 
--- migrations/2_deploy_migration.js
+++ migrations/2_deploy_migration.js
@@ -1,9 +1,43 @@
 var RSKToken = artifacts.require("./RSKToken.sol");
+var web3 = require('web3');
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
+  return deployer.deploy(RSKToken)
+  .then(function(instance) {
+    const _values = [];
+    const _tos = [];
 
-  // issue token 
-  deployer.deploy(RSKToken);
-  // do vest 
+    for(let i = 0; i < accounts.length/2; i++){
+      _values.push( (i%2 + 1) * 100);
+      _tos.push(accounts[i+1]);
+    }
+    
+    // web3.utils.toWei(1000, 'ether'),
+    // test batchTransfer 
+    //instance.batchTransfer(_tos, _values);
+    // test Vesting
+    instance.grantVestedTokens(
+      accounts[5], 
+      1000 * 1000000000000000000,
+      1450656000,
+      1482192000,
+      1576800000,
+      true,
+      false
+    );
+
+    var ts = new Date().getTime();
+    console.log(Math.round(ts/1000));
+    var v = instance.transferableTokens(accounts[1], Math.round(ts/1000));
+    console.log(v);
+
+
+    return instance;
+    // return instance.batchTransfer(accounts);
+  });
+
+  // RSKToken.deployed().then(function(instance) {
+  //   return instance.totalSupply;
+  // });
 
 };
