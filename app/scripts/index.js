4c938145687cd4ce0a2a95133729b3d06c1d278f
liuyao <liuyao@ejoy.com>
2018-10-28 16:51:52 +0800

    add send and check

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -21,8 +21,8 @@ const App = {
   start: function () {
     const self = this
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider)
+    // Bootstrap the RSKToken abstraction for Use.
+    RSKToken.setProvider(web3.currentProvider)
 
     // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function (err, accs) {
@@ -54,9 +54,9 @@ const App = {
     let rskToken
     RSKToken.deployed().then(function (instance) {
       rskToken = instance
-      return rskToken.balanceOf(account)
-      // return rskToken.balanceOf.call(account, {from: account})
-      // return meta.getBalance.call(account, { from: account })
+      // return rskToken.balanceOf(account)
+      console.log(account)
+      return rskToken.balanceOf.call(account, {from: account})
     }).then(function (value) {
       const balanceElement = document.getElementById('balance')
       balanceElement.innerHTML = value.valueOf()
@@ -85,6 +85,26 @@ const App = {
       console.log(e)
       self.setStatus('Error sending coin; see log.')
     })
+  },
+
+  checkCoin: function() {
+    const self = this
+
+    const accountVar = document.getElementById('account').value
+    this.setStatus('Initiating transaction... (please wait)')
+
+    let rskToken
+    RSKToken.deployed().then(function (instance) {
+      rskToken = instance
+      return rskToken.balanceOf.call(accountVar, { from: accountVar } )
+    }).then(function(value) {
+      const accountBalanceElement = document.getElementById('accountBalance')
+      accountBalanceElement.innerHTML = value.valueOf()
+    }).catch(function (e) {
+      console.log(e)
+      self.setStatus('Error getting balance; see log.')
+    })
+
   }
 }
 
@@ -95,7 +115,7 @@ window.addEventListener('load', function () {
   if (typeof web3 !== 'undefined') {
     console.warn(
       'Using web3 detected from external source.' +
-      ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
+      ' If you find that your accounts don\'t appear or you have 0 RskToken,' +
       ' ensure you\'ve configured that source properly.' +
       ' If using MetaMask, see the following link.' +
       ' Feel free to delete this warning. :)' +
@@ -105,13 +125,13 @@ window.addEventListener('load', function () {
     window.web3 = new Web3(web3.currentProvider)
   } else {
     console.warn(
-      'No web3 detected. Falling back to http://127.0.0.1:9545.' +
+      'No web3 detected. Falling back to http://127.0.0.1:7545.' +
       ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
       ' Consider switching to Metamask for development.' +
       ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
     )
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'))
   }
 
   App.start()
