6853e91a763b0cb94d59161aab1f013666a43ee0
liuyao <liuyao@ejoy.com>
2018-10-29 17:11:38 +0800

    fix bug

--- test/testRSKToken.js
+++ test/testRSKToken.js
@@ -8,7 +8,7 @@ contract('RskToken', async (accounts) => {
   it("should put 20000 RSKToken in the first account", async() => {
     let instance = await RSKToken.deployed();
     let balance = await instance.balanceOf.call(accounts[0]);
-    assert.equal(balance, web3.toWei('20000', 'ether'));
+    assert.equal(balance, web3.toWei('20000', 'ether').toNumber());
   })
 
 
