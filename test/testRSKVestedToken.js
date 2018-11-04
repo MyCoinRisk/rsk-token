60f280f238150f2412b26d06ca09e72fe4bcc0a0
liuyao <liuyao@ejoy.com>
2018-11-04 20:42:13 +0800

    add test

--- test/testRSKVestedToken.js
+++ test/testRSKVestedToken.js
@@ -17,7 +17,7 @@ contract('RSKVestedToken', async (accounts) => {
     });
 
     it('granter can grant tokens without vesting', async () => {
-        await token.transfer(receiver, tokenAmount, { from: granter});
+        await token.transfer(receiver, tokenAmount, { from: granter });
 
         assert.equal(await token.balanceOf(receiver), tokenAmount);
         assert.equal(await token.transferableTokens(receiver, now), tokenAmount);
@@ -26,9 +26,10 @@ contract('RSKVestedToken', async (accounts) => {
     contract('getting a revokable/non-burnable token grant', async () => {
         const cliff = 10000;
         const vesting = 20000; //seconds
+        const lockPeriod = 12000;
 
         beforeEach(async () => {
-            await token.grantVestedTokens(receiver, tokenAmount, now + cliff, now, now + cliff, now + vesting, true, false, { from: granter});
+            await token.grantVestedTokens(receiver, tokenAmount, now + lockPeriod, now, now + cliff, now + vesting, true, false, {from: granter});
         });
 
         it('tokens are received', async () => {
@@ -43,20 +44,25 @@ contract('RSKVestedToken', async (accounts) => {
             assert.equal(await token.transferableTokens(receiver, now + vesting), tokenAmount);
         });
 
+        it('has 0 transferable tokens during lockup period', async () => {
+            assert.equal(await token.transferableTokens(receiver, now + lockPeriod - 1), 0);
+            assert.equal(await token.transferableTokens(receiver, now + cliff), 0);
+        });
+
         it('throws when trying to transfer non vested tokens', async () => {
-            try{
+            try {
                 await token.transfer(accounts[7], 1, {from: receiver});
-            }catch (e) {
+            } catch (e) {
                 return assertJump(e);
             }
             assert.fail('should have thrown before');
         })
 
         it('throws when trying to transfer from non vested tokens', async () => {
-            try{
-                await token.approve(accounts[7], 1, { from: receiver });
-                await token.transferFrom(receiver, accounts[7], tokenAmount, {from: accounts[7]});
-            }catch (e) {
+            try {
+                await token.approve(accounts[7], 1, {from: receiver});
+                await token.transferFrom(receiver, accounts[7], 1, {from: accounts[7]});
+            } catch (e) {
                 return assertJump(e);
             }
             assert.fail('should have thrown before');
@@ -71,7 +77,7 @@ contract('RSKVestedToken', async (accounts) => {
         it('cannot be revoked by non granter', async () => {
             try {
                 await token.revokeTokenGrant(receiver, 0, {from: accounts[3]});
-            }catch (e) {
+            } catch (e) {
                 return assertJump(e);
             }
             assert.fail('should have thrown before');
@@ -82,6 +88,12 @@ contract('RSKVestedToken', async (accounts) => {
             await token.revokeTokenGrant(receiver, 0, {from: granter});
             assert.equal(await token.balanceOf(receiver), tokenAmount * cliff / vesting);
         })
+
+        it('can be revoked by granter during lockup period', async () => {
+            await timer(cliff);
+            await token.revokeTokenGrant(receiver, 0, {from: granter});
+            assert.equal(await token.balanceOf(receiver), tokenAmount * cliff / vesting);
+        });
 
         it('can transfer all tokens after vesting ends', async () => {
             await timer(vesting);
@@ -98,80 +110,84 @@ contract('RSKVestedToken', async (accounts) => {
 
         it('can handle composed vesting schedules', async () => {
             await timer(cliff);
-            await token.transfer(accounts[7], 12, {from: receiver});
-            assert.equal(await token.balanceOf(accounts[7]), 12);
+            assert.equal(await token.transferableTokens(receiver, now + cliff), 0);
+
+
+            await timer(lockPeriod);
+            assert.equal(await token.transferableTokens(receiver, now + lockPeriod), 30);
+
+            await token.transfer(accounts[7], 14, {from: receiver});
+            assert.equal(await token.balanceOf(accounts[7]), 14);
 
             let newNow = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
+            await token.grantVestedTokens(receiver, tokenAmount, newNow + lockPeriod, newNow, newNow + cliff, newNow + vesting, false, false, {from: granter});
 
-            await token.grantVestedTokens(receiver, tokenAmount, newNow + cliff, newNow, newNow + cliff, newNow + vesting, false, false, {from: granter});
+            await token.transfer(accounts[7], 16, {from: receiver});
+            assert.equal(await token.balanceOf(accounts[7]), tokenAmount * lockPeriod / vesting);
+            assert.equal(await token.balanceOf(receiver), tokenAmount * (2 - lockPeriod / vesting));
 
-            await token.transfer(accounts[7], 13, {from: receiver});
-            assert.equal(await token.balanceOf(accounts[7]), tokenAmount/2);
-
-            assert.equal(await token.balanceOf(receiver), 3 * tokenAmount/2);
-            assert.equal(await token.transferableTokens(receiver, newNow), 0);
             await timer(vesting);
-            await token.transfer(accounts[7], 3 * tokenAmount / 2, {from: receiver});
+            await token.transfer(accounts[7], tokenAmount * (2 - lockPeriod / vesting), {from: receiver});
             assert.equal(await token.balanceOf(accounts[7]), tokenAmount * 2);
-        })
-
-        contract('getting a non-revokable token grant', async () => {
-            const cliff = 10000;
-            const vesting = 20000; //seconds
-
-            beforeEach(async () => {
-                await token.grantVestedToken(receiver, tokenAmount, now + cliff, now, now + cliff, now + vesting, false, false, {from: granter});
-            })
-
-            it('tokens are received', async () => {
-                assert.equal(await token.balanceOf(receiver), tokenAmount);
-            })
-
-            it('throws when granter attempts to revoke', async () => {
-                try {
-                    await token.revokeTokenGrant(receiver, 0, {from: granter});
-                }catch (e) {
-                    return assertJump(e);
-                }
-                assert.fail('should have thrown before');
-            })
-        })
-
-
-        contract('getting a revokable/burnable token grant', async () => {
-            const cliff = 10000;
-            const vesting = 20000; //seconds
-            const burnAddress = '0x000000000000000000000000000000000000dead';
-
-            beforeEach(async () => {
-                await token.grantVestedTokens(receiver, tokenAmount, now + cliff, now, now + cliff, now + vesting, true, true, {from: granter});
-            })
-
-            it('tokens are received', async () => {
-                assert.equal(await token.balanceOf(receiver), tokenAmount);
-            })
-
-            it('can be revoked by granter and tokens are burned', async () => {
-                await token.revokeTokenGrant(receiver, 0, { from: granter });
-                assert.equal(await token.balanceOf(receiver), 0);
-                assert.equal(await token.balanceOf(burnAddress), tokenAmount);
-            })
-
-            it('cannot be revoked by non granter', async () => {
-                try {
-                    await token.revokeTokenGrant(receiver, 0, { from: accounts[3] });
-                } catch(error) {
-                    return assertJump(error);
-                }
-                assert.fail('should have thrown before');
-            })
-
-            it('can be revoked by granter and non vested tokens are returned', async () => {
-                await timer(cliff);
-                await token.revokeTokenGrant(receiver, 0, { from: granter });
-                assert.equal(await token.balanceOf(burnAddress), tokenAmount * cliff / vesting);
-            })
         })
     });
 
+    contract('getting a non-revokable token grant', async () => {
+        const cliff = 10000;
+        const vesting = 20000; //seconds
+        const lockPeriod = 12000;
+
+        beforeEach(async () => {
+            await token.grantVestedToken(receiver, tokenAmount, now + lockPeriod, now, now + cliff, now + vesting, false, false, {from: granter});
+        })
+
+        it('tokens are received', async () => {
+            assert.equal(await token.balanceOf(receiver), tokenAmount);
+        })
+
+        it('throws when granter attempts to revoke', async () => {
+            try {
+                await token.revokeTokenGrant(receiver, 0, {from: granter});
+            } catch (e) {
+                return assertJump(e);
+            }
+            assert.fail('should have thrown before');
+        })
+    });
+
+
+    contract('getting a revokable/burnable token grant', async () => {
+        const cliff = 10000;
+        const vesting = 20000; //seconds
+        const burnAddress = '0x000000000000000000000000000000000000dead';
+
+        beforeEach(async () => {
+            await token.grantVestedTokens(receiver, tokenAmount, now + cliff, now, now + cliff, now + vesting, true, true, {from: granter});
+        })
+
+        it('tokens are received', async () => {
+            assert.equal(await token.balanceOf(receiver), tokenAmount);
+        })
+
+        it('can be revoked by granter and tokens are burned', async () => {
+            await token.revokeTokenGrant(receiver, 0, {from: granter});
+            assert.equal(await token.balanceOf(receiver), 0);
+            assert.equal(await token.balanceOf(burnAddress), tokenAmount);
+        })
+
+        it('cannot be revoked by non granter', async () => {
+            try {
+                await token.revokeTokenGrant(receiver, 0, {from: accounts[3]});
+            } catch (error) {
+                return assertJump(error);
+            }
+            assert.fail('should have thrown before');
+        })
+
+        it('can be revoked by granter and non vested tokens are returned', async () => {
+            await timer(cliff);
+            await token.revokeTokenGrant(receiver, 0, {from: granter});
+            assert.equal(await token.balanceOf(burnAddress), tokenAmount * cliff / vesting);
+        })
+    });
 });
