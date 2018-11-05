54605fe5213483c98f33cb3905e647f648ff08c3
liuyao <liuyao@ejoy.com>
2018-11-05 20:52:21 +0800

    init new version

--- test/testRSKTimelock.js
+++
@@ -1,7 +0,0 @@
-const assertJump = require('./helpers/assertJump');
-const timer = require('./helpers/timer');
-var TokenTimelock = artifacts.require('TokenTimelock');
-
-contract('TokenTimelock', function () {
-    
-})
