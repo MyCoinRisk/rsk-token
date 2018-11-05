181f28b89a9f2afa23b6c8e8a1ac95d2630708f6
liuyao <liuyao@ejoy.com>
2018-11-05 20:25:05 +0800

    fix bug

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,32 @@ var RSKToken = artifacts.require("./RSKToken.sol");
 var RSKVestedToken = artifacts.require("./RSKVestedToken.sol")
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(RSKToken);
-  deployer.deploy(RSKVestedToken);
+
+    deployer.deploy(RSKToken);
+    deployer.deploy(RSKVestedToken);
+
+
+
+    // const amount = 10000 * 1e18;
+    // const amount = 10000;
+    // const beneficiary = accounts[1];
+    // const start = 1450656000;
+    //
+    // deployer.deploy(RSKToken)
+    //     .then(() => {
+    //         const rskToken = RSKToken.at(RSKToken.address);
+    //         console.log("rskToken:", rskToken.address);
+    //         return deployer.deploy(TokenTimelock, rskToken.address, beneficiary, start);
+    //     })
+    //     .then((t) => {
+    //         const rskToken = RSKToken.at(RSKToken.address);
+    //         console.log("tokenTimelock: ", t.address);
+    //         rskToken.transfer(t.address, amount);
+    //         return rskToken.balanceOf(t.address);
+    //     }).then((v) => {
+    //         console.log("TokenTimelock:", v);
+    //         deployer.deploy(RSKVestedToken);
+    //     });
+
 };
 
---
+++ test/testRSKTimelock.js
@@ -0,0 +1,7 @@
+const assertJump = require('./helpers/assertJump');
+const timer = require('./helpers/timer');
+var TokenTimelock = artifacts.require('TokenTimelock');
+
+contract('TokenTimelock', function () {
+    
+})
