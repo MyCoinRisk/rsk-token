ecc4ccedb7b3ad00346c623164b79addc411a28b
liuyao <liuyao@ejoy.com>
2018-11-07 15:17:46 +0800

    add test RskCrowdsale sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-var RskCrowdsaleConfig = artifacts.require("./RskCrowdsaleConfig.sol");
-var RskToken = artifacts.require("./RskToken.sol");
 var RskCrowdsale = artifacts.require("./RskCrowdsale.sol");
 
 module.exports = function(deployer, network, accounts) {
-    // deployer.deploy(RskCrowdsaleConfig);
-    // deployer.deploy(RskToken);
     deployer.deploy(RskCrowdsale, Date.now());
 };
---
+++ test/testRskCrowdsale.js
@@ -0,0 +1,9 @@
+const RskCrowdsale = artifacts.require("RskCrowdsale");
+
+contract('RskCrowdsale', async (accounts) => {
+
+    it('should put 20 billion RskToken in the first account', async () => {
+
+    });
+
+});
