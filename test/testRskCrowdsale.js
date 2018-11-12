59b0248d96366ccf84eabff28bb1ce263be0befe
liuyao <liuyao@ejoy.com>
2018-11-12 14:15:59 +0800

    add test helpers"

--- test/testRskCrowdsale.js
+++ test/testRskCrowdsale.js
@@ -1,38 +1,35 @@
 const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
-const increaseTime = require("./helper/timeTravel");
-
-const now = () => web3.eth.getBlock(web3.eth.blockNumber).timestamp;
-const day = 24 * 60 * 60;
-
 chai.use(chaiAsPromised);
 const { expect } = chai;
 
+const time = require("./helper/time");
+
 const RskCrowdsale = artifacts.require("RskCrowdsale");
+const RskToken = artifacts.require("RskToken");
 
 
 contract('RskCrowdsale', async (accounts) => {
 
     const [owner, other] = accounts;
-    console.log(owner, other);
+    // console.log(owner, other);
 
     let rskCrowdsale;
+    let aToken;
     let token;
 
     async function balanceOf(address) {
-        return Math.round(web3.fromWei(await token.balanceOf(address), "ether").toNumber());
+        return Math.round(web3.fromWei( await token.balanceOf(address), "ether").toNumber());
     }
 
     beforeEach(async () => {
-        await RskCrowdsale.new(now() + day)
-            .then((instance) => {
-                rskCrowdsale = instance;
-                console.log("address:", instance.address);
-                return instance.token();
-            }).then((_token) => {
-                token = _token;
-                console.log("token:", token);
-            });
+        rskCrowdsale = await RskCrowdsale.new(await time.latest() + time.duration.minutes(1));
+        console.log("rskCrowdsale address=", rskCrowdsale.address);
+
+        aToken = await rskCrowdsale.token();
+        token = RskToken.at(aToken);
+
+        console.log("token address=", token.address);
     });
 
     contract('tokens allocation', () => {
@@ -40,16 +37,13 @@ contract('RskCrowdsale', async (accounts) => {
             expect(await rskCrowdsale.owner()).to.equal(owner);
         });
 
-        it('should put 20 billion RskToken in the first account', async () => {
-            let amount = 20 * 1000 * 1000 * 1000;
-            console.log("token address:", token);
-            let balance = await token.balanceOf(accounts[0]);
-
-            console.log(balance);
-
-            // console.log( balanceOf(accounts[0]) );
-            // expect(balanceOf(accounts[0])).to.equal(amount);
+        it('should put 12 billion RskToken in the fifth account', async () => {
+            let amount = 12 * 1000 * 1000 * 1000;
+            expect(await balanceOf(accounts[5])).to.equal(amount);
         });
+
+        
+
     });
 
 
