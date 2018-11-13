9fb111129c6952fd40c45ae3b8c3f8b03bb5a39f
liuyao <liuyao@ejoy.com>
2018-11-13 21:38:56 +0800

    fix bug'

--- contracts/RskToken.sol
+++ contracts/RskToken.sol
@@ -4,7 +4,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
 
 contract RskToken is ERC20Capped, ERC20Detailed {
-    string private constant _name = "RSK Token";
+    string private constant _name = "MyCoinRisk";
     string private constant _symbol = "RSK";
     uint8 private constant _decimals = 18;
 
